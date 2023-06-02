ba25fdf13147a10421b1adedbea8e8d9c37786e8
João Pedro ;) <joaopedro.ribeiromg@gmail.com>
2023-06-01 22:10:40 -0300

    up

--- arquivo.js
+++ arquivo.js
@@ -1,4 +1,20 @@
 window.addEventListener("DOMContentLoaded", function(event) {
-    var text = document.querySelector("h1");
-    text.classList.add("animated");
+  var text = document.querySelector("h1");
+  text.classList.add("animated");
+});
+
+
+window.addEventListener("DOMContentLoaded", function(event) {
+  const arrowLink = document.querySelector(".arrow-link");
+  
+  arrowLink.addEventListener("click", function(event) {
+    event.preventDefault();
+    const contentSection = document.getElementById("content");
+    const contentOffset = contentSection.offsetTop;
+  
+    window.scrollTo({
+      top: contentOffset,
+      behavior: "smooth"
+    });
   });
+});
